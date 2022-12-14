53560c22f2590538e088c88bafa67923bec1d1a6
Marc Itzenthaler <itzenthaler@web-mi.de>
2022-12-14 09:45:20 +0100

    fix: merge error

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -70,8 +70,6 @@ export const Profile = () => {
 
 	const { selectableLocales } = useContext(LocaleContext);
 
-	const { selectableLocales } = useContext(LocaleContext);
-
 	useEffect(() => {
 		// Navigation is hidden and header shown on small screens if there is no enquiry yet. Should be as usual on profile routes
 		document
