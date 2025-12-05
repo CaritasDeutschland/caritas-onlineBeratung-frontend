f857269de09ab82b46f382adf9736e20e4b25b1d
leandro.silva <leandro.silva@virtual-identity.com>
2025-12-05 13:20:34 +0100

    CARITAS-765 fix: - run prettier

--- src/components/incomingVideoCall/IncomingVideoCall.tsx
+++ src/components/incomingVideoCall/IncomingVideoCall.tsx
@@ -13,21 +13,11 @@ import {
 	UserDataContext
 } from '../../globalState';
 import { useAppConfig } from '../../hooks/useAppConfig';
-import {
-	ReactComponent as CallOffIcon
-} from '../../resources/img/icons/call-off.svg';
-import {
-	ReactComponent as CallOnIcon
-} from '../../resources/img/icons/call-on.svg';
-import {
-	ReactComponent as CameraOnIcon
-} from '../../resources/img/icons/camera-on.svg';
+import { ReactComponent as CallOffIcon } from '../../resources/img/icons/call-off.svg';
+import { ReactComponent as CallOnIcon } from '../../resources/img/icons/call-on.svg';
+import { ReactComponent as CameraOnIcon } from '../../resources/img/icons/camera-on.svg';
 import { decodeUsername } from '../../utils/encryptionHelpers';
-import {
-	Button,
-	BUTTON_TYPES,
-	ButtonItem
-} from '../button/Button';
+import { Button, BUTTON_TYPES, ButtonItem } from '../button/Button';
 
 export interface VideoCallRequestProps {
 	rcGroupId: string;
